3ea50c0b537f5452d5575732dbc751157cf4d69d
sharkes2 <mwimbish27@gmail.com>
2025-02-13 23:17:41 -0500

    same

--- digital.cpp
+++ digital.cpp
@@ -1,41 +1,21 @@
 #include <wiringPi.h>
 #include <iostream>
 using namespace std; 
-  int sensor2(int state, int ms);
-
 int main() {
     // Initialize WiringPi and set up the pin numbering
     wiringPiSetup();
-    /* ___variables___ */
-    int ms;
-    int state = 0;
-   /* __Inputs/Outputs__ */
 
-    ms=pinMode( 8, INPUT);  // the senors signal line
-  /*___Fuctions details___*/     
-   int  motion= sensor2( state,ms);
- /*_____ what happes____*/
-  cout <<" the state is: "<< state  <<endl;
+    // Set GPIO pin 7 as input (WiringPi pin number)
+    
+    pinMode(2, OUTPUT);
+    int value = digitalWrite(2,HIGH);
+    if ( value == HIGH ){
+        cout << "Pin 2 is HIGH" << endl;
+        } else {
+        cout << "Pin 2 is LOW" <<endl;
+        }
         delay(100); // Delay to avoid excessive printing
     
    
     return 0;
-}
-
-int sensor2( int state , int ms){
-    int status;
-      while (true){
-      status = digitalRead(ms);   
-      if  (status >= 0 ) {           
-          cout <<" Motion detected "<<endl; 
-          state = HIGH; 
-          delay(500);       
-        }else{
-            cout<< "The action/ motion has stopped"<<endl;
-            state = LOW; 
-            delay(500);      
-        }
-      }
-      
-    return(state);
 }
