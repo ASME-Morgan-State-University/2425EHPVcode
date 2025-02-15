6acc5ee34fdac5b8f8eb002d81c351ad7b7cbdb2
sharkes2 <mwimbish27@gmail.com>
2025-02-15 00:31:43 -0500

    new update

--- sensor.cpp
+++ sensor.cpp
@@ -1,7 +1,7 @@
 #include <wiringPi.h>
 #include <iostream>
 using namespace std; 
-int sensor2(int state, int pi);
+int sensor2(int pi, int state);
 
 int main() {
     // Initialize WiringPi and set up the pin numbering
@@ -13,7 +13,7 @@ int main() {
    /* __Inputs/Outputs__ */
      pinMode(pi,INPUT); 
   /*___Fuctions details___*/  
-   mss = sensor2(state);
+   mss = sensor2(pi,state);
    /*_____ what happes____*/
     cout <<" the state is: "<< mss  <<endl;
    delay(100); // Delay to avoid excessive printing
@@ -22,7 +22,7 @@ int main() {
     return 0;
 }
 
-int sensor2(int state, int pi){
+int sensor2( int pi, int state){
   int status;
       while (true){
       status = digitalRead(pi);   
