9a0172f2e8638123e9af3ae58dc29e6a1e65bebc
sharkes2 <mwimbish27@gmail.com>
2025-02-15 15:51:24 -0500

    new

--- digital.cpp
+++ digital.cpp
@@ -10,15 +10,15 @@ int main(void) {
     
     pinMode(3, INPUT);
     int value = digitalRead(3);
-    cout << vlaue <<endl; 
+    cout << value <<endl; 
     while (true)
     {
     
-    if ( value ==  HIGH ){
+     if( value ==  HIGH ){
         cout << "Pin 2 is HIGH" << endl;
         digitalWrite(3,HIGH);
         delay(1000);
-        } if else(value == LOW) {
+        } else if(value == LOW) {
         cout << "Pin 2 is LOW" <<endl;
         digitalWrite(3,LOW);
         delay(250); // Delay to avoid excessive printing
