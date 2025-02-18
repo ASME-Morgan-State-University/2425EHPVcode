b56e914c136274682f8aa3cbae274d7df1da6a2b
sharkes2 <mwimbish27@gmail.com>
2025-02-17 22:37:03 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -11,26 +11,21 @@ int main(void) {
     pinMode(BUTTON_PIN, INPUT); // Set the button pin as an input
     pullUpDnControl(BUTTON_PIN, PUD_UP);
     int state = digitalRead(BUTTON_PIN); // Read the button state
-    int inputs [5];
-     int a;
-    int size = sizeof(inputs)/sizeof(inputs[0]);
-    std::cout << "Press the button to toggle the LED" << std::endl;
-  
-   
-   for ( int i =0 ; i < size; i++){
-     cout <<" hit the button"<< i <<":  " <<endl;
-     if (state == 1){
-      cout << "HIGH"<<endl;
-      int a = digitalWrite(state,HIGH);
-      cin >> a[i];
-     }else { 
-      cout <<"LOW"<<endl;
-      a = digitalWrite(state,LOW);
-      cin >> a[i];
+    const int arraySize= 10;
+    int currentIndex = 0;
+
+    cout << "______Starting_____" << endl;
+
+    state[currentIndex]= state;
+     for (int i =0; i < state; i++ ){
+          cout << " HIGH"<< state[i]<< endl;
+      for(int j =0; j > state; j++){
+        cout << " LOW "<<state[j]<< endl;
+      }
      }
-   }
-
-   return 0;
-
-
+     currentIndex =(currentIndex +1) % arraySize;
+     delay(100);
+  
+    return 0;
+    
 }
