d590969bd9c2f5dc15da0825b175d9516288a359
sharkes2 <mwimbish27@gmail.com>
2025-02-17 20:30:39 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -8,15 +8,13 @@ int main(void) {
 
     // Set GPIO pin 7 as input (WiringPi pin number)
     const int BUTTON_PIN = 18; // BCM pin connected to the button
-    const int LED_PIN = 17;    // BCM pin connected to the LED
     pinMode(BUTTON_PIN, INPUT); // Set the button pin as an input
-    pinMode(LED_PIN, OUTPUT);   // Set the LED pin as an output
     pullUpDnControl(BUTTON_PIN, PUD_UP);
     digitalRead(BUTTON_PIN); // Read the button state
 
     std::cout << "Press the button to toggle the LED" << std::endl;
     while (true) {
-      digitalWrite(LED_PIN, !digitalRead(BUTTON_PIN));
+      cout<< "the Button is:   " << BUTTON_PIN <<endl;
       delay(50);
 
   
