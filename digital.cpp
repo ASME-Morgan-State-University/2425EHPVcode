0e0876d08bf62c97b16f331f43b4a48bb5feeba9
sharkes2 <mwimbish27@gmail.com>
2025-02-17 20:23:31 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -16,14 +16,11 @@ int main(void) {
 
     std::cout << "Press the button to toggle the LED" << std::endl;
     while (true) {
-      if(digitalWrite(LED_PIN, !digitalRead(BUTTON_PIN))){
-        cout << "HIGH"<<endl;
-      }else{ 
-        cout <<"LOW"<<endl;
-      }
-    delay(50); // Small delay to debounce the button
-    }
+      digitalWrite(LED_PIN, !digitalRead(BUTTON_PIN));
+      delay(50):
 
+  
+      }
 
     return 0;
 }
