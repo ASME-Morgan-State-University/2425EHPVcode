06f125ea043274c9f01bb56f1af5978c21ce04dd
sharkes2 <mwimbish27@gmail.com>
2025-02-19 20:43:29 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -13,16 +13,30 @@ int main(void) {
 
     pinMode(BUTTON_PIN, INPUT); // Set the button pin as an input
     pullUpDnControl(BUTTON_PIN, PUD_UP);
-  
+    vector<int> buttonStates;
+    cout << "______Starting_____" << endl;
+    for (int i = 0; i < 10; ++i) {
+      int buttonState = digitalRead(BUTTON_PIN);
+      buttonStates.push_back(buttonState);
+
+      std::cout << "Button state " << i + 1 << ": " << buttonState << std::endl;
+
+      // Wait for a short period before reading again
+      delay(1000);  // 1 second
+  }
+
+      // Display the stored button states
+       std::cout << "Stored button states:" << std::endl;
+     for (int i = 0; i < buttonStates.size(); ++i) {
+      std::cout << "State " << i + 1 << ": " << buttonStates[i] << std::endl;
+  }
+
 
   
-   while (true)
-   {
-  
    
-    cout << "______Starting_____" << endl;
-    int state = digitalRead(BUTTON_PIN); // Read the button state
-    state [currentIndex]= state;
+    cout << "______Ending_____" << endl;
+    /*int state = digitalRead(BUTTON_PIN); // Read the button state
+    state [currentIndex] = state;
      for (int i =0; i < arraySize; i++ ){
             if ( state[i] == 1){
               cout <<"HIGH"<<endl;
@@ -33,7 +47,7 @@ int main(void) {
      
      currentIndex =(currentIndex +1) % arraySize;
      delay(100);
-        }
+        }*/
     return 0;
     
 }
