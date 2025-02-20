37ab3995818ccaf00059be0846d8ae4956640cba
sharkes2 <mwimbish27@gmail.com>
2025-02-19 21:52:12 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -7,47 +7,29 @@ int main(void) {
     wiringPiSetupGpio();
 
     const int BUTTON_PIN = 18;
-    int arraySize=10;
-    int currentIndex = 0;
-    int state[arraySize];
+    int buttonState; 
+    int numEntries;
+    
+    
+   pinMode(BUTTON_PIN, INPUT); // Set the button pin as an input
+   pullUpDnControl(BUTTON_PIN, PUD_UP);
+   buttonState = digitalRead(BUTTON_PIN);
 
-    pinMode(BUTTON_PIN, INPUT); // Set the button pin as an input
-    pullUpDnControl(BUTTON_PIN, PUD_UP);
-    vector<int> buttonStates;
-    cout << "______Starting_____" << endl;
-    for (int i = 0; i < 10; ++i) {
-      int buttonState = digitalRead(BUTTON_PIN);
-      buttonStates.push_back(buttonState);
-
-      std::cout << "Button state " << i + 1 << ": " << buttonState << std::endl;
-
-      // Wait for a short period before reading again
-      delay(1000);  // 1 second
-  }
-
-      // Display the stored button states
-       std::cout << "Stored button states:" << std::endl;
-     for (int i = 0; i < buttonStates.size(); ++i) {
-      std::cout << "State " << i + 1 << ": " << buttonStates[i] << std::endl;
-  }
-
-
-  
+   struct Userdata
+   {
+    int buttonState;
+   }
+   Userdata(numEntries)
+   for (int i=0; i< numEntries;i++ ){
+     Userdata[i].buttonState = digitalRead(BUTTON_PIN);
+     cout<< "Button state for user"<< i + 1 << ": " <<Userdata[i].buttonState << endl;
+     delay(1000);
+    }
+    cout <<"User data entered: "<< endl;
+    for( int i=0; i < numEntries; i++){
+      cout <<" User "<< i+1 << ": name = "<< Userdata[i].name<< ", Button State = "<<Userdata[i].buttonState<<endl; 
+    }
    
-    cout << "______Ending_____" << endl;
-    /*int state = digitalRead(BUTTON_PIN); // Read the button state
-    state [currentIndex] = state;
-     for (int i =0; i < arraySize; i++ ){
-            if ( state[i] == 1){
-              cout <<"HIGH"<<endl;
-            }else{
-              cout <<"LOW"<<endl;
-            }
-          }
-     
-     currentIndex =(currentIndex +1) % arraySize;
-     delay(100);
-        }*/
     return 0;
     
 }
