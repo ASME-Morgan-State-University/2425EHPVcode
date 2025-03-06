6acc7e334519b40a9d42fa6fa0c2ae45788fadfe
sharkes2 <mwimbish27@gmail.com>
2025-03-05 21:37:09 -0500

    old

--- digital.cpp
+++ digital.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 #include <ctime>
-#include<cstdlib>
-#include <iomanip>
-#include <cmath>
 #include <random>
 using namespace std;
-float getRandNum( float v);
+double getRandNum();
 int main (){  
    
-       float v;
-      getRandNum(v);
-       cout<< v << endl;
+    
+      getRandNum();
+       cout << getRandNum() << endl;
        
    
       
 }
- float getRandNum( float v){
-
-   //const int sizeArray = 1;
-  // double numberArray[sizeArray];
-  double value;
-   srand(time(0));
-   std::random_device rd;
-   std::mt19937 gen(rd());    
-   float m=pow(10,2);
-   float n= round(gen() * m) / m;
-
-
-   /*for(int i = 0; i < sizeArray; i++){
-      numberArray[i] =num;
+  double getRandNum(){
+ srand(time(0));// time generate number   
+     // Set up the random number generator
+     std::random_device rd;
+     std::mt19937 generator(rd());
+     std::uniform_real_distribution<double> distribution(0.0, 10.0); // Range [0.0, 10.0]
+     // Generate a random number
+     double raw_number = distribution(generator);
+     // Round the number to 3 decimal places
+     double rounded_number = std::round(raw_number * 1000.0) / 1000.0;
+     const int sizeArray = 1;
+     double numberArray[sizeArray];
+     double value;
+     for(int i = 0; i < sizeArray; i++){
+      numberArray[i] =  rounded_number;
       value = numberArray[i];
      // cout<< fixed<<setprecision(3)<<numberArray[i]<<"   ";// Pritning the random numbers
-   }*/
-   return(n);
+   }
+   return(value);
   } 
